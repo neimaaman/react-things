edead7c3693d30c145f81b8f73fbc6e5b9a1580b
neimaaman <neimaaman1234@gmail.com>
2024-02-28 12:49:24 -0500

    added more components

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,4 +1,4 @@
-export default function Profile() {
+ export default function Profile() {
     return (
       <img
         src="https://i.imgur.com/lICfvbD.jpg"
@@ -7,13 +7,13 @@ export default function Profile() {
     );
   }
 
-  export default function Profile() {
+ function Profile1() {
    
     return  <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
   }
 
 
-  function Profile() {
+  function Profile2() {
     return (
       <img
         src="https://i.imgur.com/QIrZWGIs.jpg"
@@ -22,20 +22,9 @@ export default function Profile() {
     );
   }
   
-  export default function Gallery() {
-    return (
-      <section>
-        <h1>Amazing scientists</h1>
-        <profile />
-        <profile />
-        <profile />
-      </section>
-    );
-  }
+
+  
+  
   
 
-  export default function Congrajulation () {
-
-    return <h1> Good Job! </h1>
-  }
-  
+ 
