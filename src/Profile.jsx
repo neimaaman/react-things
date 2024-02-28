7afed62c94f850ce27b67af64adb44169628730a
neimaaman <neimaaman1234@gmail.com>
2024-02-28 11:17:54 -0500

    components exercise

---
+++ src/Profile.jsx
@@ -0,0 +1,41 @@
+export default function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/lICfvbD.jpg"
+        alt="Aklilu Lemma"
+      />
+    );
+  }
+
+  export default function Profile() {
+   
+    return  <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+  }
+
+
+  function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/QIrZWGIs.jpg"
+        alt="Alan L. Hart"
+      />
+    );
+  }
+  
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <profile />
+        <profile />
+        <profile />
+      </section>
+    );
+  }
+  
+
+  export default function Congrajulation () {
+
+    return <h1> Good Job! </h1>
+  }
+  
