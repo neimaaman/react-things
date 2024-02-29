d74028030786afe553c515f6969613bf514fbdec
neimaaman <neimaaman1234@gmail.com>
2024-02-28 19:38:34 -0500

    added JS expression

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,5 +1,9 @@
+const baseUrl = 'https://i.imgur.com/';
 const person = {
     name: 'Gregorio Y. Zara',
+    imageURL: 'https://i.imgur.com/7vQD0fPs.jpg',
+    imageId: '7vQD0fP',
+    imageSize: 'b',
     theme: {
       backgroundColor: 'black',
       color: 'pink'
@@ -15,6 +19,12 @@ const person = {
           src="https://i.imgur.com/7vQD0fPs.jpg"
           alt="Gregorio Y. Zara"
         />
+        <img
+            className="avatar"
+            src={baseUrl + person.imageId + person.imagwSize + '.jpg'}
+            alt={person.name}
+            />
+
         <ul>
           <li>Improve the videophone</li>
           <li>Prepare aeronautics lectures</li>
