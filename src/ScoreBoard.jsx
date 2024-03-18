d073cf940e0a3693041c19209a1726d9801ef033
neima <neimaaaaman@gmail.com>
2024-03-17 23:51:56 -0400

    // 15-updating-objects-in-state - Challenge 1 of 3: Fix the incorrect state updates

---
+++ src/ScoreBoard.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+export default function Scoreboard() {
+    const [player, setPlayer] = useState({
+        firstName: 'Ranjani',
+        lastName: 'Shettar',
+        score: 10,
+    });
+
+    function handlePlusClick(e) {
+        setPlayer({ ...player,
+            score: player.score+ 1
+
+        })
+    }
+    function handleFirstNameChange(e) {
+        setPlayer({
+            ...player,
+            firstName: e.target.value,
+        });
+    }
+
+    function handleLastNameChange(e) {
+        setPlayer({
+            ...player,
+            lastName: e.target.value
+        });
+    }
+
+    return (
+        <>
+            <label>
+                Score: <b>{player.score}</b>
+                {' '}
+                <button onClick={handlePlusClick}>
+                    +1
+                </button>
+            </label>
+            <label>
+                First name:
+                <input
+                    value={player.firstName}
+                    onChange={handleFirstNameChange}
+                />
+            </label>
+            <label>
+                Last name:
+                <input
+                    value={player.lastName}
+                    onChange={handleLastNameChange}
+                />
+
+            </label>
+
+            <p>
+                {player.firstName}{' '}
+                {player.lastName}{' '}
+                ({player.score})
+            </p>
+        </>
+    );
+}
+
+// 15-updating-objects-in-state - Challenge 1 of 3: Fix the incorrect state updates
