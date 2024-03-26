c6edad25c9b4bfe246c5fa4acd621f0155086761
neima <neimaaaaman@gmail.com>
2024-03-25 21:49:15 -0400

    // 14-updating-arrays-without-mutation - challenge 2 of 4 Remove an item from the shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,12 @@ import ColorSwitch from "./colorSwitch";
 import Gallery from "./GalleryThree";
 import Form from "./Form";
 import FeedbackFormTwo from "./FeedbackFormTwo";
+import Canvas from "./Canvas.jsx";
+import Canvas2 from "./Canvas2.jsx";
+import ShoppingCart from "./Products.jsx";
+import ShoppingCart2 from "./Product2.jsx";
+import TaskApp from "./IntialTodos.jsx";
+import TaskApp2 from "./TaskApp.jsx";
 
 
 
@@ -46,6 +52,7 @@ function App() {
             <StoryTray stories={stories} setStories={() => console.log("hello")} />
             <StoryTray stories={stories} setStories= {setStories}/>
             {/*<StoryTray stories={stories} setStories={()=> {}}/>*/}
+
         
       
 
@@ -72,6 +79,12 @@ function App() {
         <Gallery/>
         <Form/>
         <FeedbackFormTwo/>
+        <Canvas />
+        <Canvas2 />
+        <ShoppingCart />
+        <ShoppingCart2 />
+        <TaskApp />
+        <TaskApp2 />
       </div>
       
        
